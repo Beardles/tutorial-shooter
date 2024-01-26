24d6d6a3cf8b45e3bceabe0e7ce124d8f1cfb98f
John Beadles <jbeardles@gmail.com>
2024-01-26 16:17:42 -0500

    Finish creating custom scenes tutorial.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
+signal laser(pos)
+signal grenade
+
 var can_laser: bool = true
 var can_grenade: bool = true
-
-signal laser
-signal grenade
 
 func _process(_delta):
 	
@@ -15,15 +15,20 @@ func _process(_delta):
 	
 	# laser shooting input
 	if Input.is_action_pressed("primary action") and can_laser:
-		$LaserTimer.start()
+		# randomly selected a marker 2D for the laser starting position
+		var laser_markers = $LaserStartPositions.get_children()
+		var selected_laser = laser_markers[randi() % laser_markers.size()]
 		can_laser = false
-		laser.emit()
+		$LaserTimer.start()
+		# emit the position we selected
+		laser.emit(selected_laser.global_position)
 		
 	# grenade launching input
 	if Input.is_action_pressed("secondary action") and can_grenade:
-		$GrenadeTimer.start()
 		can_grenade = false
-		grenade.emit()
+		$GrenadeTimer.start()
+		var pos = $LaserStartPositions.get_children()[0].global_position
+		grenade.emit(pos)
 
 
 
