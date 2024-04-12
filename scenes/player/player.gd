3a22da2ae8bd23a228f2fa59d89d3513424bd708
John Beadles <jbeardles@gmail.com>
2024-04-12 09:13:25 -0400

    Get laser and grenade shooting in player direction.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,19 +1,23 @@
 extends CharacterBody2D
 
-signal laser(pos)
-signal grenade
+signal laser(pos, direction)
+signal grenade(pos, direction)
 
 var can_laser: bool = true
 var can_grenade: bool = true
 
 func _process(_delta):
-	
 	# input
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * 500
 	move_and_slide()
 	
+	# rotate
+	look_at(get_global_mouse_position())
+		
 	# laser shooting input
+	var player_direction = (get_global_mouse_position() - position).normalized()
+	
 	if Input.is_action_pressed("primary action") and can_laser:
 		# randomly selected a marker 2D for the laser starting position
 		var laser_markers = $LaserStartPositions.get_children()
@@ -21,14 +25,15 @@ func _process(_delta):
 		can_laser = false
 		$LaserTimer.start()
 		# emit the position we selected
-		laser.emit(selected_laser.global_position)
+		laser.emit(selected_laser.global_position, player_direction)
 		
 	# grenade launching input
 	if Input.is_action_pressed("secondary action") and can_grenade:
 		can_grenade = false
 		$GrenadeTimer.start()
 		var pos = $LaserStartPositions.get_children()[0].global_position
-		grenade.emit(pos)
+
+		grenade.emit(pos, player_direction)
 
 
 
