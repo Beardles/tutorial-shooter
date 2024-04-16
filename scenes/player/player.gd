ef0596fddf39ff63b0400edd88e54958691c84d4
John Beadles <jbeardles@gmail.com>
2024-04-15 21:39:55 -0400

    Do some particle stuff.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -19,6 +19,7 @@ func _process(_delta):
 	var player_direction = (get_global_mouse_position() - position).normalized()
 	
 	if Input.is_action_pressed("primary action") and can_laser:
+		$Explosion.emitting = true
 		# randomly selected a marker 2D for the laser starting position
 		var laser_markers = $LaserStartPositions.get_children()
 		var selected_laser = laser_markers[randi() % laser_markers.size()]
@@ -26,6 +27,7 @@ func _process(_delta):
 		$LaserTimer.start()
 		# emit the position we selected
 		laser.emit(selected_laser.global_position, player_direction)
+		
 		
 	# grenade launching input
 	if Input.is_action_pressed("secondary action") and can_grenade:
